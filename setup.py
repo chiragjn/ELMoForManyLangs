57b7712cd124e5e4cf28c5b91fb8a48de6650a2f
Chirag Jain <jain.chirag925@gmail.com>
2019-07-09 15:09:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setuptools.setup(
   version="0.0.2",
   packages=setuptools.find_packages(),
   install_requires=[
-    "torch",
-    "h5py",
-    "numpy",
+    "torch==1.1.0",
+    "h5py==2.9.0",
+    "numpy==1.16.4",
     "overrides",
   ],
   classifiers=[
